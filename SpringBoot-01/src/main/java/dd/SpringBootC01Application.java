0852c75cea4b2baa7ae4afd77b3f92c85dbf54c1
huaaijia <huaaijia@letv.com>
2016-10-19 18:37:38 +0800

    DD-SpringBoot-01

---
+++ SpringBoot-01/src/main/java/dd/SpringBootC01Application.java
@@ -0,0 +1,12 @@
+package dd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootC01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootC01Application.class, args);
+	}
+}
