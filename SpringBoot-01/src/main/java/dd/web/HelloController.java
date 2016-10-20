a1a292a718524e002fa08076d33bae58f1131265
huaaijia <huaaijia@letv.com>
2016-10-20 17:24:11 +0800

    DD-SpringBoot-02

--- SpringBoot-01/src/main/java/dd/web/HelloController.java
+++ SpringBoot-01/src/main/java/dd/web/HelloController.java
@@ -1,7 +1,9 @@
 package dd.web;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by huaaijia on 2016/10/19.
@@ -11,5 +13,13 @@ public class HelloController {
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
+    }
+
+    @RequestMapping("/")
+    public ModelAndView index(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://blog.didispace.com");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return new ModelAndView("index");
     }
 }
