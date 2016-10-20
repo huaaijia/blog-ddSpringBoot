a42f98984a47cf0655114cd0d708f431c6ed07df
huaaijia <huaaijia@letv.com>
2016-10-20 19:13:42 +0800

    DD-SpringBoot-05 06

--- SpringBoot-01/src/main/java/dd/SpringBootC01Application.java
+++ SpringBoot-01/src/main/java/dd/SpringBootC01Application.java
@@ -2,8 +2,10 @@ package dd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"dd.*"})
 public class SpringBootC01Application {
 
 	public static void main(String[] args) {
